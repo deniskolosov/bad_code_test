66ea4d5599f069df6b1f22f9532470ae8283dec9
Denis Kolosov <denis.kolosov@gmail.com>
2020-04-11 16:18:39 +0300

    Change user model link to one-to-one

--- models.py
+++ models.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Users(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Юзер')
+class AccountUser(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, verbose_name='Пользователи')
     inn = models.IntegerField(verbose_name='ИНН')
-    account = models.FloatField(verbose_name='Счёт')
+    account = models.FloatField(verbose_name='Счeта')
 
     def __str__(self):
-        return '{id} {inn}'.format(id=str(self.id), inn=self.inn)
+        return 'id={id}, inn={inn}'.format(id=str(self.id),
+                                           inn=self.inn)
